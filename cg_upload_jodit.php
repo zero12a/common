39d936c6f1e7619dee7709670117cd48d7637bce
kims man <zero12a@gmail.com>
2020-10-21 14:13:41 +0900

    filestore

--- cg_upload_jodit.php
+++ cg_upload_jodit.php
@@ -5,6 +5,10 @@ header("Content-Type: text/html; charset=UTF-8");
 $CFG = require_once("./include/incConfig.php");
 
 require_once("./include/incUtil.php");
+require_once("./include/incFile.php");
+require_once("./include/incUser.php");
+require_once("./include/incRequest.php");
+require_once("./include/incSec.php");
 
 //$RtnVal = array();
 //$RtnVal2 = array();
@@ -28,6 +32,21 @@ $RtnVal = array();
 
 //var_dump($_FILES);
 
+$REQ["storeid"] = reqGetString("storeid",100);
+
+$storeType = $CFG["CFG_FILESTORE"][$REQ["storeid"]]["STORETYPE"];
+$acl = $CFG["CFG_FILESTORE"][$REQ["storeid"]]["ACL"]; //private, public-read, public-read-write
+
+
+//프라이빗이면 로그인 검사하기.
+if( ($acl == "" || strtolower($acl) == "private") && !isLogin())MsgExit("로그인 후 파일 접근이 가능합니다. (ACL private)");
+
+//입력값 검증
+if( $REQ["storeid"] == "" )MsgExit("storeid 정보가 입력되지 않았습니다.");
+if( $CFG["CFG_FILESTORE"][$REQ["storeid"]]["STORETYPE"] == "" )MsgExit("storeid의 STORETYPE 정보를 찾을수 없습니다.");
+
+
+
 $fileNm = $_FILES["files"]["name"][0];
 //$_FILES["files"]["type"];
 //$_FILES["files"]["size"];
@@ -39,11 +58,13 @@ $savePath = $CFG["CFG_UPLOAD_DIR"] . $saveFileNm;
 //$_FILES["files"]["error"];
 if(isAllowExtension($fileNm,$CFG["CFG_IMG_EXT"])){
         
-    if(move_uploaded_file($tmpPath, $savePath)){
+    //if(move_uploaded_file($tmpPath, $savePath)){
+
+    if(moveFileStore($CFG["CFG_FILESTORE"][$REQ["storeid"]], $tmpPath, $saveFileNm)){
         //echo "/up/" . $saveFileNm;      
         $RtnVal["success"] = true;
         $RtnVal["data"]["files"] = array($saveFileNm);
-        $RtnVal["data"]["baseurl"] = "http://localhost:8040/up/";
+        $RtnVal["data"]["baseurl"] = "/common/cg_read_filestore.php?fileinfo=0|" . $REQ["storeid"] . "|" . $saveFileNm . "|" . $saveFileNm;   ////timestamp yymmddhhmiss|storeid|sever file name|origin file name
 
         $RtnVal["data"]["messages"] = array("(msg)Upload success.");
         $RtnVal["data"]["isImages"] = array(true);
@@ -55,7 +76,7 @@ if(isAllowExtension($fileNm,$CFG["CFG_IMG_EXT"])){
 
         $RtnVal["data"]["messages"] = array("(msg)Upload error.");
         $RtnVal["data"]["isImages"] = array();
-        $RtnVal["data"]["error"] ="File dont move to upload folder. tmpPath=" . $tmpPath . ", savepath=" . $savePath;
+        $RtnVal["data"]["error"] ="File dont move to upload folder. storeid=" . $REQ["storeid"] . ", tmpPath=" . $tmpPath . ", svrNm=" . $saveFileNm;
         $RtnVal["data"]["code"] = "500";
     }
 }else{
